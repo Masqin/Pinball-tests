65b172ff196aeb7c49823015f98757158596fdac
Masqin <102848136+Masqin@users.noreply.github.com>
2022-04-02 07:57:42 +0100

    First basic test

--- __main__.py
+++ __main__.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://pinball.etrash.pro")
 
 driver.implicitly_wait(0.5)
 
-assert "Pinball Leaderboard" in driver.title
+print(driver.title)
+
+assert driver.title == "Pinball Leaderboard"
 
 driver.quit()
